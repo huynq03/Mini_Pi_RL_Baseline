8108c372426d3cee693b17023dbee72faba2fbda
zhoumi-biped <153519746@qq.com>
2024-10-09 19:24:37 +0800

    Edit cycle time in sim2sim.

--- humanoid/scripts/sim2sim.py
+++ humanoid/scripts/sim2sim.py
@@ -151,8 +151,8 @@ def run_mujoco(policy, cfg):
                 eu_ang = quaternion_to_euler_array(quat)
                 eu_ang[eu_ang > math.pi] -= 2 * math.pi
 
-                obs[0, 0] = math.sin(2 * math.pi * count_lowlevel * cfg.sim_config.dt  / 0.64)
-                obs[0, 1] = math.cos(2 * math.pi * count_lowlevel * cfg.sim_config.dt  / 0.64)
+                obs[0, 0] = math.sin(2 * math.pi * count_lowlevel * cfg.sim_config.dt  / 0.5)
+                obs[0, 1] = math.cos(2 * math.pi * count_lowlevel * cfg.sim_config.dt  / 0.5)
                 obs[0, 2] = cmd.vx * cfg.normalization.obs_scales.lin_vel
                 obs[0, 3] = cmd.vy * cfg.normalization.obs_scales.lin_vel
                 obs[0, 4] = cmd.dyaw * cfg.normalization.obs_scales.ang_vel
